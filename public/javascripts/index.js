52b3629313b889e4a47586b549e88b901b6d5cdd
Cristian Mundo <00086917@uca.edu.sv>
2021-06-19 20:34:25 -0600

    correccion de bug en boton y se a;adio descargas

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,63 +2,64 @@ var jsonArr = [];
 var newArr = [];
 var createJSON = []
 var txtDelimitador = '';
+
+document.getElementById("savejsonbtn").addEventListener('click',clicked);
+
 document.getElementById('inputfile-txt')
     .addEventListener('change', function () {
         txtDelimitador = document.getElementById('delimitador').value;
-        if(txtDelimitador.length === 0){
+        if (txtDelimitador.length === 0) {
             alert('Se necesita una key');
             location.reload();
-        }
-        var fr = new FileReader();
-        fr.onload = function () {
-            document.getElementById('output')
-                .textContent = fr.result;
-            jsonArr.push(fr.result.split('\n'));
-            jsonArr[0].forEach(element => {
-                newArr.push(element.split(txtDelimitador))
-            });
+        } else {
+            var fr = new FileReader();
+            fr.onload = function () {
+                document.getElementById('output')
+                    .textContent = fr.result;
+                jsonArr.push(fr.result.split('\n'));
+                jsonArr[0].forEach(element => {
+                    newArr.push(element.split(txtDelimitador))
+                });
 
-            for (let i = 0; i < newArr.length; i++) {
+                for (let i = 0; i < newArr.length; i++) {
 
-                createJSON = [...createJSON, {
-                    documento: newArr[i][0],
-                    nombre: newArr[i][1],
-                    apellido: newArr[i][2],
-                    credit_card: newArr[i][3],
-                    tipo: newArr[i][4],
-                    telefono: newArr[i][5]
-                }]
+                    createJSON = [...createJSON, {
+                        documento: newArr[i][0],
+                        nombre: newArr[i][1],
+                        apellido: newArr[i][2],
+                        credit_card: newArr[i][3],
+                        tipo: newArr[i][4],
+                        telefono: newArr[i][5]
+                    }]
 
+                }
+                document.getElementById('outputjson')
+                    .textContent = JSON.stringify(createJSON, null, '\t')
+
+                var textToDownload = JSON.stringify(createJSON, null, '\t')
+                download(textToDownload, 'txt-converted.json', 'application/json');
             }
-            document.getElementById('outputjson')
-                .textContent = JSON.stringify(createJSON, null, '\t')
 
-
+            fr.readAsText(this.files[0]);
         }
-        var btn = document.createElement("BUTTON");   // Create a <button> element
-        btn.innerHTML = "Save Json file";
-        btn.id = "btn-savejson"
-        btn.addEventListener('click', clicked)            // Insert text
-        document.getElementById("savejsonbtn").appendChild(btn);
-        fr.readAsText(this.files[0]);
+
     })
 
 function clicked() {
+    console.log('clicked');
     const key = document.getElementById('key').value;
-    document.getElementById('btn-savejson').addEventListener('click', function () {
-        fetch('http://localhost:3000/generarJWT',{
-            method:'POST',
-            headers: {
+    fetch('http://localhost:3000/generarJWT', {
+        method: 'POST',
+        headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-            body: JSON.stringify({
-                json: createJSON,
-                key 
-            })
-        }).then(res => res.json() )
-          .then( json => document.getElementById('textoJWT').textContent = json.newJwt);
-    })
+        body: JSON.stringify({
+            json: createJSON,
+            key
+        })
+    }).then(res => res.json())
+        .then(json => document.getElementById('textoJWT').textContent = json.newJwt);
 }
 let txtBody = '';
 let delimitador = '';
@@ -66,7 +67,7 @@ let delimitador = '';
 document.getElementById('inputfile').addEventListener('change', function () {
 
     delimitador = document.getElementById('input-del').value
-    
+
     if (delimitador.length > 0) {
         var reader = new FileReader();
         reader.onload = function () {
@@ -84,7 +85,7 @@ document.getElementById('inputfile').addEventListener('change', function () {
             var btnText = document.createElement("BUTTON");
             btnText.innerHTML = "Save txt file"
             btnText.id = "btn-savetxt"
-            btnText.addEventListener('click', download(txtBody, 'json.txt', 'text/plain'))
+            btnText.addEventListener('click', download(txtBody, 'json-converted.txt', 'text/plain'))
             document.getElementById("savetxtbtn").appendChild(btnText);
 
         }
@@ -101,7 +102,6 @@ function download(content, fileName, contentType) {
     var file = new Blob([content], { type: contentType });
     a.href = URL.createObjectURL(file);
     a.download = fileName;
-    console.log('clicked');
     a.click();
     URL.revokeObjectURL(a.href);
 }
